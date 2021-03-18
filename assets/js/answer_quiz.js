a98bc30c1858c502c1a07374c77b0068fea5ee75
Christopher Chang <chiggiechang@gmail.com>
2021-03-18 00:16:57 -0700

    Finish basic comparison quizzes

--- assets/js/answer_quiz.js
+++ assets/js/answer_quiz.js
@@ -11,6 +11,19 @@ function renderLastScore() {
 }
 renderLastScore();
 
+function colorLabel(label) {
+  const input = label.querySelector("input.quiz-circle");
+  const score = parseInt(input.value);
+
+  if (score === 1) {
+    label.classList.add("quiz-option-correct");
+  } else if (input.checked) {
+    label.classList.add("quiz-option-incorrect");
+  } else {
+    label.classList.remove("quiz-option-correct", "quiz-option-incorrect");
+  }
+}
+
 document.getElementById("form1").onsubmit = function () {
   const checkedCircles = Array.from(document.querySelectorAll("input.quiz-circle:checked"));
   // console.log(checkedCircles);
@@ -37,17 +50,85 @@ document.getElementById("form1").onsubmit = function () {
 
   /// Highlight correct and false answers
   for (const label of document.querySelectorAll("label.quiz-option")) {
-    const input = label.querySelector("input.quiz-circle");
-    const score = parseInt(input.value);
+    colorLabel(label);
+  }
 
-    if (score === 1) {
-      label.classList.add("quiz-option-correct");
-    } else if (input.checked) {
-      label.classList.add("quiz-option-incorrect");
-    } else {
-      label.classList.remove("quiz-option-correct", "quiz-option-incorrect");
-    }
+  /// Show explanation elements
+  for (const expl of document.querySelectorAll("#form1 p.explanation")) {
+    expl.style.display = "inline-block";
   }
 
   return false; // required to not refresh the page; just leave this here
 }; //this ends the submit function
+
+/// Implement the password-choice functionality
+for (const choiceQuiz of document.querySelectorAll(".password-choice")) {
+  choiceQuiz.onsubmit = choiceQuizOnSubmit;
+}
+
+function choiceQuizOnSubmit() {
+  for (const option of this.querySelectorAll("label.quiz-option")) {
+    colorLabel(option);
+  }
+
+  for (const expl of this.querySelectorAll(".explanation")) {
+    expl.style.display = "inline-block";
+  }
+
+  return false;
+}
+
+// This function dynamically creates a password quiz form
+function createPasswordQuiz(
+  name,
+  answerIndex,
+  options,
+  explanation
+) {
+  const quizzesParent = document.querySelector("#password-quizzes");
+
+  const rootForm = document.createElement("form");
+  quizzesParent.appendChild(rootForm);
+  rootForm.classList.add("password-choice", "stack-options", "dynamic-quiz");
+  rootForm.onsubmit = choiceQuizOnSubmit;
+
+  const optionsDiv = document.createElement("div");
+  rootForm.appendChild(optionsDiv);
+
+  function createOption(text) {
+    const option = document.createElement("label");
+    optionsDiv.appendChild(option);
+    option.classList.add("quiz-option");
+
+    const input = document.createElement("input");
+    option.appendChild(input);
+    input.classList.add("quiz-circle");
+    input.name = name;
+    input.type = "radio";
+    option.append(text);
+
+    return input;
+  }
+
+  for (const [i, option] of options.entries()) {
+    const elem = createOption(option);
+    if (i === answerIndex) {
+      elem.value = "1";
+    }
+  }
+
+  const explElem = document.createElement("p");
+  optionsDiv.appendChild(explElem);
+  explElem.classList.add("explanation");
+  explElem.textContent = explanation;
+
+  const submitDiv = document.createElement("div");
+  rootForm.appendChild(submitDiv);
+  submitDiv.classList.add("partially-aligned-button");
+
+  const submitButton = document.createElement("button");
+  submitDiv.appendChild(submitButton);
+  submitButton.type = "submit";
+  submitButton.value = "Submit";
+  submitButton.textContent = "Submit";
+}
